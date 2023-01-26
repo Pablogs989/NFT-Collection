636343493f0f176490ee8a31ac1e3099a9922a4e
Pablogs989 <pablogonsan2001@gmail.com>
2023-01-26 20:49:31 +0100

    new contract with metadata

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x4C79adf1B7384cD9dAB7D3D31212652f0CB777ff";
+const WHITELIST_CONTRACT_ADDRESS = "0x7B9Fe0A08A5d8B6a6c9dF75D68971001B1FebEC5";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-ivory-five.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
